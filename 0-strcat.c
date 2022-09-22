709cefde8b9f5c4ed91855490f48760b5e2a7682
wende <wendebuzu@gmail.com>
2022-09-22 16:51:14 -0700

    concatination

---
+++ 0-strcat.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "0-strcat.h"
+#include <string.h>
+
+char *_strcat(char *dest, char *src)
+
+{
+		    return  strcat(dest,src);
+
+}
---
+++ 0-strcat.h
@@ -0,0 +1,19 @@
+#ifndef strcont_DOT_H    /* This is an "include guard" */
+
+#define strcont_DOT_H    /* prevents the file from being included twice. */
+
+                     /* Including a header file twice causes all kinds */
+
+                     /* of interesting problems.*/
+
+
+
+/**
+ *
+ *  * This is a function declaration.
+ *
+ *   * It tells the compiler that the function exists somewhere.
+ *
+ *    */
+char *_strcat(char *dest, char *src);
+#endif /* strcont_DOT_H */
